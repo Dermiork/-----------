40e75817f6567044e4ee1bc93d47b8143aa038eb
Dermiork <dermiork@gmail.com>
2026-03-08 17:41:42 +0200

    first commit

---
+++ library-data.js
@@ -0,0 +1,19 @@
+const libraryData = {
+  cycles: [
+    {
+      id: "cycle-1",
+      title: "Средь теней собора и лунного трона",
+      description: "Наш основной цикл.",
+      volumes: [
+        {
+          id: "volume-1",
+          title: "Том 1",
+          chapters: [
+            { id: "chapter-1", title: "Пролог", content: "..." },
+            { id: "chapter-2", title: "Глава 1", content: "..." }
+          ]
+        }
+      ]
+    }
+  ]
+};
